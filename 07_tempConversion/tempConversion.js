c8d1943ad34b50e3734a995ee6d3af39a4815016
Joshua Jimenez <jjimenez.work2@gmail.com>
2022-05-30 16:09:21 -0500

    Completed 07 Temp Conversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(fah) {
+  let cel = (fah - 32) * 5 / 9;
+  return +cel.toFixed(1);
 };
 
-const ctof = function() {
-
+const ctof = function(cel) {
+  let fah = cel * 9 / 5 + 32;
+  return +fah.toFixed(1);
 };
 
 // Do not edit below this line
