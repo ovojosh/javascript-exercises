e6b408a473f9cd7511d300ff0a104c54136cb610
Joshua Jimenez <jjimenez.work2@gmail.com>
2022-05-30 15:36:37 -0500

    Completed 05 Sum All

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
+    //typeof is finding out if min or max are numbers/integers
+    if(typeof min !== 'number' || typeof max !== 'number') {
+        return 'ERROR';
+    }
 
+    //If min and max are negatives, then return 'ERROR'
+    if (min < 0 || max < 0) {
+        return 'ERROR';
+    }
+
+    //If min is greater than max, swap them
+    if (min > max) {
+        [min, max] = [max, min];
+    }
+
+    let sum = 0;
+    //Adding from min to max, and all numbers between min and max
+    for (let i = min; i <= max; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
