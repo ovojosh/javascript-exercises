5b42a1bf981ddcdccdef783c7a5fec04282b78f8
Joshua Jimenez <jjimenez.work2@gmail.com>
2022-05-30 14:55:56 -0500

    Completed 04 Remove Array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(...args) {
+    //The first element in the removeFromArray function is an array.
+    //So we separate args[0] into an array.
+    const array = args[0];
+    //Next, we filter out the elements that are included in the function arguments, but not in the array.
+    return array.filter(el => !args.includes(el))
 };
 
 // Do not edit below this line
